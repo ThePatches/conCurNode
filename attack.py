a5d5975112e62c6096eb81fa7441a8cf906e2eea
Patrick Taylor <warder05@gmail.com>
2014-07-28 17:31:29 -0400

    Modified attack.py to be a little more helpful

--- attack.py
+++ attack.py
@@ -1,8 +1,14 @@
 import http.client
+import json
 
 def doAttack(currThread):
 	conn = http.client.HTTPConnection("localhost:1337")
 	conn.request("GET", "/add?name=Larry&age=22&job=Sweeper");
 	response = conn.getresponse()
-	print(currThread);
-	print(response.read())
+	respBody = response.read()
+	someVar = json.JSONDecoder().decode(respBody.decode("utf-8"));
+	#print(currThread);
+	print(currThread + ": " + str(len(someVar)))
+
+if __name__ == "__main__":
+	doAttack("MainThread")
