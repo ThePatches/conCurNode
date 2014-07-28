83c378bce0336f9cd079a59d462442b06e27bf92
Patrick Taylor <warder05@gmail.com>
2014-07-28 17:38:57 -0400

    Use object counts to prove that there are concurrency problems with using a global NodeJS object

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from doThreads import MyThread
 
-thread1 = MyThread(1, "thread-1", 2)
-thread2 = MyThread(2, "thread-2", 2)
+someThreads = []
+threadNum = 10
 
-thread1.start()
-thread2.start()
+for x in range(0, threadNum):
+	someThreads.append(MyThread(x + 1, "thread-" + str(x + 1), 10))
+
+for nThread in someThreads:
+	nThread.start()
+
+#thread1 = MyThread(1, "thread-1", 2)
+#thread2 = MyThread(2, "thread-2", 2)
+
+#thread1.start()
+#thread2.start()
 
 print("Exiting Main Thread")
